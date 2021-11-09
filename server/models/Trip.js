98910f34ef85b5aacf3d8d1f3f1977129470763e
ljbrewer <ljbrewer.lb@gmail.com>
2021-11-09 13:39:05 -0800

    new stuff

--- server/models/Trip.js
+++ server/models/Trip.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 
+
 const tripSchema = new Schema({
     name: {
         type: String,
@@ -7,4 +8,29 @@ const tripSchema = new Schema({
         unique: true,
         trim: true,
     },
+    datetostartTrip:{
+        type: Date,
+        trim: true,
+    },
+    startLocation:{
+        type: String,
+        trim: true,
+    },
+   sLat:{
+        type: String,
+    },
+   sLon: {
+        type: String,
+    },
+    endLocation:{
+        type: String,
+        trim: true,
+    },
+    eLat: {
+        type: String,
+    },
+    eLon: {
+        type: String,
+    },
+    
 })
