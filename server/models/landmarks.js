28ab2af69149fd875c59bf1039bdfcb51fd1a01d
ljbrewer <ljbrewer.lb@gmail.com>
2021-11-09 13:03:09 -0800

    some updates to files

---
+++ server/models/landmarks.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const profileSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+
+}
+)
+            
---
+++ server/models/waypoints.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+
+const profileSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },})
