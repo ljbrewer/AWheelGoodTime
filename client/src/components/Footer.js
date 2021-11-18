249690b94c7dbf923843fbc32245af482be68939
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-17 21:42:58 -0800

    add header and footer components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+const styles = {
+    foot: {
+        background: '#f9b713',
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+},
+};
+
+function Footer() {
+    return (
+        <div style={styles.foot}>
+        
+        
+            <h1>footer</h1>
+       
+        
+   
+        </div>
+    );
+}
+
+export default Footer;
